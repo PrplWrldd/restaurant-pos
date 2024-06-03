55ba8a4826fa4f397f12787f61d005d10ba7e435
kyrool <kfitri559@gmail.com>
2024-06-04 01:39:02 +0800

    add: new dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    container: {
-      center:true,
-      padding:'2rem'
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        container: {
+            center: true,
+            padding: "2rem",
+        },
+        extend: {},
     },
-    extend: {},
-  },
-  plugins: [],
-}
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: ["light"],
+    },
+};
